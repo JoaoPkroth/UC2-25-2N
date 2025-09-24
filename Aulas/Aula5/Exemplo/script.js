7416e58a739e209715a68f6580233645f48cb17c
JoaoPKroth <krothkoro@gmail.com>
2025-09-23 21:44:22 -0300

    Aula5

---
+++ Aulas/Aula5/Exemplo/script.js
@@ -0,0 +1,17 @@
+const got = "Game of Thrones"
+const numeroTemporadas = 8
+console.log(got)
+console.log(numeroTemporadas)
+console.log(typeof(got))
+console.log(typeof(numeroTemporadas))
+let novaVarial
+console.log(novaVarial)
+novaVarial = 22
+console.log(novaVarial)
+novaVarial = undefined
+console.log(novaVarial)
+
+let minhaVarial
+console.log(minhaVarial)
+minhaVarial = null
+console.log(minhaVarial)
---
+++ Aulas/Aula5/Exercico/script.js
@@ -0,0 +1,10 @@
+//Exercicio aula 5
+
+const idade = prompt("Qual sua idade?")
+console.log(typeof(idade))
+//Number() conversor de string para number
+const idadeNumber = Number(idade)
+console.log(typeof(idadeNumber))
+//toString conversor de number para string
+const idadeTexto = idadeNumber.toString
+console.log(typeof(idadeTexto))
