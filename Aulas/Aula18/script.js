522c6c2548ab54ffd7f2dd5f2815eda2a29c60c0
JoaoPKroth <krothkoro@gmail.com>
2025-11-12 21:52:28 -0300

    Aula18

--- Aulas/Aula18/script.js
+++ Aulas/Aula18/script.js
@@ -19,7 +19,9 @@ console.log(
 }
 
 adicionandoAmigo(pessoa) 
-*/
+
+
+
 const personagens = [
  {
     nome : "Yuji Itadori",
@@ -41,7 +43,7 @@ const personagens = [
 }
 ]
 
-function adicionarPersonagem(personagens) {
+function adicionarPersonagem(personagem) {
     personagens.push(personagem)
     console.table(`Personagem ${personagem.nome} adicionado com sucesso!`)
 }
@@ -55,7 +57,140 @@ function buscarPersonagem(nome) {
             console.table(personagem)
             return personagem
         } else {
-            console.table(`Personagem "${nome}" não encontrado.`)
+            console.log(`Personagem "${nome}" não encontrado.`)
             return null
         }
 }
+
+function listarPersonagens () {
+    console.table("Lista de personagens de Jujutso Kaisen:")
+    personagens.forEach(p => {
+        console.table(`Nome: ${p.nome}`)
+        console.table(`Idade: ${p.idade}`)
+        console.table(`Estilo: ${p.estilo}`)
+        console.table(`Jujutso: ${p.jujutso.join(", ")}`)
+    })
+}
+
+listarPersonagens()
+
+adicionarPersonagem({
+    nome : "Nobara Kugisaki",
+    idade :15,
+    estilo : "Usuária de Feitiço de boneco",
+    jujutso : ["Resonace", "Fulgor Negro", "Grampo de Cabelo", "Técnica do Boneco de Palha"]
+})
+
+buscarPersonagem("Gojo")
+buscarPersonagem("Sukuna")
+
+listarPersonagens()
+*/
+/*
+1. Leia o código.
+c
+a) O que vai ser
+impresso no console?
+
+
+const filme = {
+    nome : "Auto da Compadecida",
+    ano : 2000,
+    elenco : [
+        "Matheus Nachtergale", "Selton Mello", "Denise Fraga", "Virginia Cavendish"
+    ],
+    transmissoesHoje : [
+        {Canal : "Telecine", horario : "21"},
+        {Canal : "Canal Brasil", horario : "14h"}
+    ]
+}
+
+console.log(filme.elenco[0])
+console.log(filme.elenco[filme.elenco.length - 1])
+console.log(filme.transmissoesHoje[2])
+
+//Imprime Matheus Nachtergale e Virginia Cavendish
+
+/*
+2. Leia o código.
+
+a) O que vai ser
+impresso no console?
+
+b) O que faz a sintaxe dos três pontos antes do nome de um objeto?
+
+
+const cachorro = {
+    nome : "Juca",
+    idade : 3,
+    raca : "SRD"
+}
+
+const gato = {...cachorro, nome : "Juba"}
+
+const tartaruga = {...gato, nome : gato.nome.replaceAll("a", "o")}
+
+console.log(cachorro)
+console.log(gato)
+console.log(tartaruga)
+
+//Imprime: Nome: 'Juca', Idade: 3, Raça: SRD / Nome: 'Juba', Idade: 3, Raça: SRD / Nome: 'Jubo', Idade: 3, Raça: SRD
+
+//A sintaxe de propagação é comumente usada para fazer cópias superficiais de objetos
+
+/*
+3. Leia o código.
+
+a) O que vai ser
+impresso no console?
+
+b) Explique o valor impresso no
+console. Você sabe por que isso
+aconteceu?
+
+
+function minhaFuncao(objeto, propriedade) {
+    return objeto[propriedade]
+}
+
+const pessoa = {
+    nome: "Caio",
+    idade: 23,
+    backender: false
+}
+
+console.log(minhaFuncao(pessoa, "backender"))
+console.log(minhaFuncao(pessoa, "altura"))
+
+//Vai ser impresso: False e Undefined
+//
+
+/*
+1. Resolva os passos a seguir:
+a) Crie um objeto. Ele deve conter duas propriedades: nome (string)
+e apelidos (um array que sempre terá exatamente três apelidos).
+Depois, escreva uma função que recebe como entrada um objeto e
+imprime uma mensagem no modelo abaixo:
+
+
+//Exemplo de entrada
+const pessoa = {
+    nome : "Lucas Gabriel", 
+    apelidos: ["Mestre", "Prof", "Lu"]
+}
+
+//Exemplo de saída 
+"Eu sou lucas, mas pode me chamar de: Mestre, Prof ou Lu"
+*/
+
+const pessoa = {
+    nome: "João",
+    apelidos: ["Koro", "KoroXaein", "Jão"]
+}
+
+function apresentarPessoa(Eu) {
+    console.log(`Eu sou ${Eu.nome}, mas pode me chamar de: ${Eu.apelidos[0]}, ${Eu.apelidos[1]}, ou ${Eu.apelidos[2]}`)
+}
+
+apresentarPessoa(pessoa)
+
