32b991157b4ab942fd941c166ca24339b5294f5b
JoaoPKroth <krothkoro@gmail.com>
2025-11-18 21:51:39 -0300

    Aula20

---
+++ Aulas/Aula20/script.js
@@ -0,0 +1,64 @@
+/*
+//Exercicio
+
+let matrizExercicio = [
+    [0,0,0],
+    [0,0,1],
+    [0,0,0]
+]
+console.table(matrizExercicio)
+
+
+//Exemplo
+let matrizEx = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+for (let i=0; i <matrizEx.length; i++) {
+    for(let j=0; j<matrizEx[i].length;j++) {
+        console.log(matrizEx[i][j])
+    }
+}
+
+let matrizEx1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+    
+    let soma = 0
+    for (let i= 0; i < matrizEx1.length; i++) {
+    for (let j = 0; j < matrizEx1[i].length; j++) {
+    soma += matrizEx1[i][j]
+     }
+}
+console.log("Soma dos elementos:", soma)
+
+let matriz = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
+]
+let soma = 0
+for (let i=0; i <matriz.length; i++) {
+    for(let j=0; j <matriz[i].length; j++) {
+        soma += matriz[i][j]
+        
+    }
+}
+console.log(soma)
+*/
+let matrizNaval = [
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0]
+]
